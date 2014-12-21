75a97bedaed3ba518e5d0d09355f296341d56204
Catherine Johnson <cfjohnson@earthlink.net>
2014-12-21 10:37:05 -0700

    updated with topic models, controller, and view framework.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221171127) do
+ActiveRecord::Schema.define(version: 20141221172221) do
 
   create_table "authors", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -31,5 +31,8 @@ ActiveRecord::Schema.define(version: 20141221171127) do
 
   add_index "authors", ["email"], name: "index_authors_on_email", unique: true
   add_index "authors", ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
+
+# Could not dump table "blogposts" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
