ecc3bda7b0fd10e8c440b629c9d9ce64d8fd2815
Catherine Johnson <cfjohnson@earthlink.net>
2014-12-21 10:19:41 -0700

    updated signin with names, and added container for body.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221161404) do
+ActiveRecord::Schema.define(version: 20141221171127) do
 
   create_table "authors", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141221161404) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "authors", ["email"], name: "index_authors_on_email", unique: true
